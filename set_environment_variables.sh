a5fd6ac8c6930ceec8b6bc3bb1423cb46ee7a826
Chukwuemeka Chukwu <chukwuemeka.chukwu@eng.uniben.edu>
2023-06-25 01:32:07 +0100

    Update set_environment_variables.sh

--- set_environment_variables.sh
+++ set_environment_variables.sh
@@ -1,5 +1,5 @@
 # replace placeholders and rename this file to `set_environment_variables.sh`
-export COMET_ML_API_KEY="YOUR_COMET_ML_API_KEY"
+export COMET_ML_API_KEY="mdCtskYdEweIpXg1d67T2B02a"
 export COMET_ML_WORKSPACE="YOUR_COMET_ML_WORKSPACE"
 
 export CEREBRIUM_API_KEY="YOUR_CEREBRIUM_API_KEY"
